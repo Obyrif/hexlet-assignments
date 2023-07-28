5056a876520eedcb4e7a067001dd24d5b66ca15e
@hexlet/cli <support@hexlet.io>
2023-07-28 23:41:36 +0800

    submit java-collections-ru/lists_28.07.23_20.28.56

--- java-collections-ru/lists_28.07.23_20.28.56/src/main/java/exercise/App.java
+++ java-collections-ru/lists_28.07.23_20.28.56/src/main/java/exercise/App.java
@@ -6,30 +6,27 @@ import java.util.List;
 
 // BEGIN
 
+
 public class App {
     public static boolean scrabble(String letters, String word) {
         // Приводим все символы к нижнему регистру
         letters = letters.toLowerCase();
         word = word.toLowerCase();
 
-        // Создаем массив для хранения количества каждого символа в наборе
-        int[] letterCount = new int[26];
+        // Создаем список для хранения символов в наборе
+        ArrayList<Character> letterList = new ArrayList<>();
 
-        // Заполняем массив количеством символов в наборе
-        for (int i = 0; i < letters.length(); i++) {
-            char c = letters.charAt(i);
-            letterCount[c - 'a']++;
+        // Заполняем список символами из набора
+        for (char c : letters.toCharArray()) {
+            letterList.add(c);
         }
 
         // Проверяем, достаточно ли символов для составления слова
-        for (int i = 0; i < word.length(); i++) {
-            char c = word.charAt(i);
-            if (letterCount[c - 'a'] == 0) {
+        for (char c : word.toCharArray()) {
+            if (!letterList.remove(Character.valueOf(c))) {
                 return false;
             }
-            letterCount[c - 'a']--;
         }
-
         return true;
     }
 }
