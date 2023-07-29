1656f1d26a8f1128eb8545582cd0f1933ad21d8f
@hexlet/cli <support@hexlet.io>
2023-07-29 11:49:15 +0800

    submit java-collections-ru/lists_28.07.23_20.28.56

--- java-collections-ru/lists_28.07.23_20.28.56/src/main/java/exercise/App.java
+++ java-collections-ru/lists_28.07.23_20.28.56/src/main/java/exercise/App.java
@@ -7,19 +7,14 @@ import java.util.List;
 // BEGIN
 public class App {
     public static boolean scrabble(String letters, String word) {
-        // Приводим все символы к нижнему регистру
         letters = letters.toLowerCase();
         word = word.toLowerCase();
 
-        // Создаем список для хранения символов в наборе
         ArrayList<Character> letterList = new ArrayList<>();
 
-        // Заполняем список символами из набора
         for (char c : letters.toCharArray()) {
             letterList.add(c);
         }
-
-        // Проверяем, достаточно ли символов для составления слова
         for (char c : word.toCharArray()) {
             if (!letterList.remove(Character.valueOf(c))) {
                 return false;
