59965ce34b1441d82679dfeb9b321304722f0616
@hexlet/cli <support@hexlet.io>
2023-08-02 18:11:44 +0800

    submit java-collections-ru/generics

--- java-collections-ru/generics/src/main/java/exercise/App.java
+++ java-collections-ru/generics/src/main/java/exercise/App.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 
 // BEGIN
 public class App {
-    public static List<Map<String, String>> findWhere(List<Map<String, String>> books, HashMap<String, String> line) {
+    public static List<Map<String, String>> findWhere(List<Map<String, String>> books, Map<String, String> line) {
         List<Map<String, String>> result = new ArrayList<>();
 
         for (Map<String, String> book: books) {
