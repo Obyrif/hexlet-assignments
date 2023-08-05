4d6e6e4f9d6697099282ff37fe50475748f5d1eb
@hexlet/cli <support@hexlet.io>
2023-08-05 18:50:40 +0800

    submit java-collections-ru/tests

---
+++ java-collections-ru/tests/src/test/java/exercise/AppTest.java
@@ -0,0 +1,23 @@
+package exercise;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import java.util.List;
+import java.util.Arrays;
+import java.util.ArrayList;
+import org.junit.jupiter.api.Test;
+
+class AppTest {
+
+    @Test
+    void testTake(List<Integer> list, int numberElement) {
+        // BEGIN
+        List<Integer> unput = new ArrayList<>();
+        unput.add(1);
+        unput.add(2);
+        unput.add(3);
+        int number = 2;
+        List<Integer> result = App.take(unput,number);
+        ssertThat(result).containsExactly(1, 2);
+        // END
+    }
+}
