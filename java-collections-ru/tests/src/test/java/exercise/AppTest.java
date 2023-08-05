f9712e4463c5dd254e9057b28ff9d0f840b088d2
@hexlet/cli <support@hexlet.io>
2023-08-05 21:41:02 +0800

    submit java-collections-ru/tests

--- java-collections-ru/tests/src/test/java/exercise/AppTest.java
+++ java-collections-ru/tests/src/test/java/exercise/AppTest.java
@@ -4,16 +4,48 @@ package exercise;
 import java.util.List;
 //import java.util.ArrayList;
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import java.util.Arrays;
+import java.util.List;
+
 
 class AppTest {
-
     @Test
-    void testTake(List<Integer> list, int number) {
-        // BEGIN
+    void testTake() {
+        // Arrange
         List<Integer> inputList = Arrays.asList(1, 2, 3, 4, 5);
         int numberOfElements = 3;
+
+        // Act
         List<Integer> result = Take.take(inputList, numberOfElements);
-        assertEquals(Arrays.asList(1, 2), result);
-        // END
+
+        // Assert
+        assertEquals(Arrays.asList(1, 2, 3), result);
+    }
+
+    @Test
+    void testTake_EmptyList() {
+        // Arrange
+        List<Integer> inputList = Arrays.asList();
+        int numberOfElements = 3;
+
+        // Act
+        List<Integer> result = Take.take(inputList, numberOfElements);
+
+        // Assert
+        assertEquals(Arrays.asList(), result);
+    }
+
+    @Test
+    void testTake_NumberOfElementsGreaterThanListSize() {
+        // Arrange
+        List<Integer> inputList = Arrays.asList(1, 2, 3);
+        int numberOfElements = 5;
+
+        // Act
+        List<Integer> result = Take.take(inputList, numberOfElements);
+
+        // Assert
+        assertEquals(Arrays.asList(1, 2, 3), result);
     }
 }
