e3799ac9d68327c81e4791698cea74d8b46ea3bc
@hexlet/cli <support@hexlet.io>
2023-08-06 17:37:54 +0800

    submit java-collections-ru/tests

--- java-collections-ru/tests/src/test/java/exercise/AppTest.java
+++ java-collections-ru/tests/src/test/java/exercise/AppTest.java
@@ -9,52 +9,47 @@ import org.junit.jupiter.api.Test;
 class AppTest {
 
     @Test
-    void testTake() {
-        // BEGIN
-        @Test
-        void rightTest() {
-            List<Integer> elements = Arrays.asList(1, 2, 3, 4, 5);
-            int count = 3;
+    void rightTest() {
+        List<Integer> elements = Arrays.asList(1, 2, 3, 4, 5);
+        int count = 3;
 
-            List<Integer> expected = Arrays.asList(1, 2, 3);
-            List<Integer> actual = Implementations.right(elements, count);
+        List<Integer> expected = Arrays.asList(1, 2, 3);
+        List<Integer> actual = Implementations.right(elements, count);
 
-            Assertions.assertEquals(expected, actual);
-        }
-
-        @Test
-        void wrong1Test() {
-            List<Integer> elements = Arrays.asList(1, 2, 3, 4, 5);
-            int count = 3;
-
-            List<Integer> expected = Arrays.asList(1, 2, 3);
-            List<Integer> actual = Implementations.wrong1(elements, count);
-
-            Assertions.assertEquals(expected, actual);
-        }
-
-        @Test
-        void wrong2Test() {
-            List<Integer> elements = Arrays.asList(1, 2, 3, 4, 5);
-            int count = 3;
-
-            List<Integer> expected = Arrays.asList(1, 2, 3);
-            List<Integer> actual = Implementations.wrong2(elements, count);
-
-            Assertions.assertEquals(expected, actual);
-        }
-
-        @Test
-        void wrong3Test() {
-            List<Integer> elements = Arrays.asList(1, 2, 3, 4, 5);
-            int count = 3;
-
-            List<Integer> expected = Arrays.asList(1, 2, 3);
-            List<Integer> actual = Implementations.wrong3(elements, count);
-
-            Assertions.assertEquals(expected, actual);
-        }
-        // END
+        Assertions.assertEquals(expected, actual);
     }
 
+    @Test
+    void wrong1Test() {
+        List<Integer> elements = Arrays.asList(1, 2, 3, 4, 5);
+        int count = 3;
+
+        List<Integer> expected = Arrays.asList(1, 2, 3);
+        List<Integer> actual = Implementations.wrong1(elements, count);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void wrong2Test() {
+        List<Integer> elements = Arrays.asList(1, 2, 3, 4, 5);
+        int count = 3;
+
+        List<Integer> expected = Arrays.asList(1, 2, 3);
+        List<Integer> actual = Implementations.wrong2(elements, count);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void wrong3Test() {
+        List<Integer> elements = Arrays.asList(1, 2, 3, 4, 5);
+        int count = 3;
+
+        List<Integer> expected = Arrays.asList(1, 2, 3);
+        List<Integer> actual = Implementations.wrong3(elements, count);
+
+        Assertions.assertEquals(expected, actual);
+    }
+}
 }
