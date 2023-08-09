6619ddd5844124749a39d8b2542b0b33281b4b52
@hexlet/cli <support@hexlet.io>
2023-08-09 18:58:06 +0800

    submit java-collections-ru/lambdas

--- java-collections-ru/lambdas/src/main/java/exercise/App.java
+++ java-collections-ru/lambdas/src/main/java/exercise/App.java
@@ -3,5 +3,24 @@ package exercise;
 import java.util.Arrays;
 
 // BEGIN
+public class App {
+    public static String[][] enlargeArrayImage(String[][] array) {
+        int rows = array.length;
+        int cols = array[0].length;
 
+        String[][] image = new String[rows * 2][cols * 2];
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                String pix = array[i][j];
+
+                image[i * 2][j * 2] = pix;
+                image[i * 2 + 1][j * 2] = pix;
+                image[i * 2][j * 2 + 1] = pix;
+                image[i * 2 + 1][j * 2 + 1] = pix;
+            }
+        }
+        return image;
+    }
+}
 // END
--- java-collections-ru/lambdas/src/test/java/exercise/AppTest.java
+++ java-collections-ru/lambdas/src/test/java/exercise/AppTest.java
@@ -4,5 +4,33 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 // BEGIN
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class AppTest {
+
+    @Test
+    public void testEnlargedArrayImage() {
+        String[][] originalArray = {
+                {"*", "*", "*", "*"},
+                {"*", " ", " ", "*"},
+                {"*", " ", " ", "*"},
+                {"*", "*", "*", "*"},
+        };
+
+        String[][] expectedArray = {
+                {"*", "*", "*", "*", "*", "*", "*"},
+                {"*", "*", "*", "*", "*", "*", "*"},
+                {"*", "*", " ", " ", " ", "*", "*"},
+                {"*", "*", " ", " ", " ", "*", "*"},
+                {"*", "*", " ", " ", " ", "*", "*"},
+                {"*", "*", "*", "*", "*", "*", "*"},
+                {"*", "*", "*", "*", "*", "*", "*"},
+        };
+
+        String[][] enlargedArray = App.enlargeArrayImage(originalArray);
+        assertArrayEquals(expectedArray, enlargedArray);
+    }
+}
 
 // END
