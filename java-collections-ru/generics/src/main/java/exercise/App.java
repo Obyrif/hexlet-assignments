2313b935dd3dfa30d8307ce39e016be39c6235a8
Obydennov Dmitrii <dima-obydennov@mail.ru>
2023-09-13 14:19:26 +0800

    oop1

--- java-collections-ru/generics/src/main/java/exercise/App.java
+++ java-collections-ru/generics/src/main/java/exercise/App.java
@@ -6,7 +6,6 @@ import java.util.ArrayList;
 
 // BEGIN
 public class App {
-
     public static <K, V> List<Map<K, V>> findWhere(List<Map<K, V>> books, Map<K, V> criteria) {
         List<Map<K, V>> result = new ArrayList<>();
         for (Map<K, V> book : books) {
