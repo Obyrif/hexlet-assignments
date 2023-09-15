60f6b162c05feef18c6ad155be3932067ea38075
@hexlet/cli <support@hexlet.io>
2023-09-15 16:48:25 +0800

    submit java-oop-ru/subtyping

--- java-oop-ru/subtyping/src/main/java/exercise/App.java
+++ java-oop-ru/subtyping/src/main/java/exercise/App.java
@@ -1,26 +1,16 @@
 package exercise;
 
 import java.util.Map;
-import java.util.Map.Entry;
-import java.util.HashMap;
+import java.util.TreeMap;
 
 // BEGIN
 public class App {
     public static void swapKeyValue(KeyValueStorage storage) {
         Map<String, String> existingData = storage.toMap();
-        Map<String, String> swappedMap = new HashMap<>();
-
-        for (Map.Entry<String, String> entry : existingData.entrySet()) {
-            swappedMap.put(entry.getValue(), entry.getKey());
+        TreeMap<String, String> s2 = new TreeMap<>(existingData);
+        for (Map.Entry<String, String> entry : s2.entrySet()) {
+            existingData.put(entry.getValue(), entry.getKey());
         }
-
-        for (Map.Entry<String, String> entry : swappedMap.entrySet()) {
-            storage.set(entry.getKey(), entry.getValue());
-        }
-        Set<Map<String, String>> s = new HashSet<>();
-        s.add(existingData);
-        s.add(swappedMap);
     }
 }
-
 // END
