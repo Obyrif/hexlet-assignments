3dffac86f602a80c76a4e062026eacbe9ce07539
@hexlet/cli <support@hexlet.io>
2023-09-15 16:53:42 +0800

    submit java-oop-ru/subtyping

--- java-oop-ru/subtyping/src/main/java/exercise/App.java
+++ java-oop-ru/subtyping/src/main/java/exercise/App.java
@@ -1,15 +1,17 @@
 package exercise;
 
-import java.util.Map;
 import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
 
-// BEGIN
 public class App {
     public static void swapKeyValue(KeyValueStorage storage) {
         Map<String, String> existingData = new HashMap<>();
+        TreeMap<String, String> s = new TreeMap<>(existingData);
         for (Map.Entry<String, String> entry : existingData.entrySet()) {
             existingData.put(entry.getValue(), entry.getKey());
         }
     }
 }
+
 // END
