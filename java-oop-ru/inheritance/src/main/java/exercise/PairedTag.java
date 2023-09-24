f6f4dea7291e5e0f56c6f5e8647c4e7a50100199
@hexlet/cli <support@hexlet.io>
2023-09-24 19:38:53 +0300

    submit java-oop-ru/inheritance

--- java-oop-ru/inheritance/src/main/java/exercise/PairedTag.java
+++ java-oop-ru/inheritance/src/main/java/exercise/PairedTag.java
@@ -19,7 +19,7 @@ public class PairedTag extends Tag {
         sb.append("<").append(getName());
 
         for (Map.Entry<String, String> entry : getAttribute().entrySet()) {
-            sb.append(" ").append(entry.getKey()).append("=\"").append(entry.getValue()).append("\">").append(string);
+            sb.append(" ").append(entry.getKey()).append("=\"").append(entry.getValue()).append("\"").append(string);
         }
         for (Tag child : list) {
             sb.append(child.toString());
