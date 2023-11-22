e69108509fdb7bca8ac9fc086d7570cb48d680bb
@hexlet/cli <support@hexlet.io>
2023-11-22 20:59:01 +0300

    submit java-web-ru/handlers

--- java-web-ru/handlers/src/main/java/exercise/App.java
+++ java-web-ru/handlers/src/main/java/exercise/App.java
@@ -18,15 +18,8 @@ public final class App {
         var app = Javalin.create(config -> {
             config.plugins.enableDevLogging();
         });
-        app.get("/phones", ctx -> {
-            ctx.json(phones);
-            System.out.println("GET /phones requested");
-        });
-
-        app.post("/domains", ctx -> {
-            ctx.json(domains);
-            System.out.println("POST /domains requested");
-        });
+        app.get("/phones", ctx -> ctx.json(phones).result("GET /phones"));
+        app.post("/domains", ctx -> ctx.json(domains).result("GET /domains"));
         return app;
         // END
     }
