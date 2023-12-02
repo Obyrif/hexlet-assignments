110f34088011fb7d618498ab559f150c320bf74d
@hexlet/cli <support@hexlet.io>
2023-12-02 13:47:04 +0300

    submit java-web-ru/session

---
+++ java-web-ru/session/src/main/java/exercise/controller/SessionsController.java
@@ -0,0 +1,40 @@
+package exercise.controller;
+
+import java.util.Collections;
+import exercise.dto.MainPage;
+import exercise.dto.LoginPage;
+import exercise.repository.UsersRepository;
+import static exercise.util.Security.encrypt;
+
+import exercise.util.NamedRoutes;
+import exercise.util.Security;
+import io.javalin.http.Context;
+
+public class SessionsController {
+
+    // BEGIN
+    public static void build(Context ctx) {
+        var page = new LoginPage(null, null);
+        ctx.render("build.jte", Collections.singletonMap("page", page));
+    }
+
+    public static void create(Context ctx) {
+        var name = ctx.formParam("name");
+        var password = Security.encrypt(ctx.formParam("password"));
+        var user = UsersRepository.findByName(name);
+        if (user != null && password.equals(user.getPassword())) {
+            ctx.sessionAttribute("currentUser", name);
+            ctx.redirect(NamedRoutes.rootPath());
+        } else {
+            var error = "Wrong username or password";
+            var page = new LoginPage(name, error);
+            ctx.render("build.jte", Collections.singletonMap("page", page));
+        }
+    }
+
+    public static void destroy(Context ctx) {
+        ctx.sessionAttribute("currentUser", null);
+        ctx.redirect(NamedRoutes.rootPath());
+    }
+    // END
+}
